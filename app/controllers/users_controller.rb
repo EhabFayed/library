2f052aa408927b618f8a8e2a941f05abaaaed4c9
EhabFayed <ehabahmed2233@gmail.com>
2024-02-04 02:35:03 +0200

    every author and his books

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,23 @@ class UsersController <ApplicationController
             render 'new'
         end
     end
+    def edit
+        @user=User.find(params[:id])
+    end
+
+    def update
+        @user=User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success]= "ur account has been updated"
+            redirect_to books_path
+        else
+            render'edit'
+        end
+    end
+    def show
+        @user=User.find(params[:id])
+    end
+
     private
     def user_params
         params.require(:user).permit(:username,:email,:password)
